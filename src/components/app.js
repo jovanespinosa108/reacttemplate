28196c54ea0820b91922eece0f73704d85a3ee9d
jovanespinosa108@gmail.com <jovanespinosa108@gmail.com>
2022-08-20 11:34:19 -0500

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Bootstrap</h2>
+        <h3>Ready to start</h3>
+      </div>
+    );
+  }
+}
